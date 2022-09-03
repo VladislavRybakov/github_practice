34dd68aa0817c89ca6b1431a23ebc907fc2ff0f2
VladislavRybak <rybakovve@mail.ru>
2022-09-03 22:29:37 +0300

    comparison of library radon transformation and discrete radon transformation

--- final.py
+++ final.py
@@ -1,47 +1,41 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
-from skimage.transform import rotate
+from skimage.transform import rotate, radon
 from skimage.transform import rescale
 
 
-def radon_transform(PATH):
-    img = cv2.imread(PATH, 0)  # read image in grayscale
-    # img = rescale(img, scale=1.4)
-    sinogram = radon(img, theta=np.arange(89., 90.))
-
-    # plt.imshow(sinogram, cmap=plt.cm.Greys_r, aspect='auto')
-
-    # plt.plot(sinogram)
-
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 8))
-    ax1.imshow(img, cmap=plt.cm.Greys_r)
-    ax2.imshow(sinogram, cmap=plt.cm.Greys_r, aspect='auto')
-
-    plt.show()
+def radon_transform(image): # https://scikit-image.org/docs/stable/auto_examples/transform/plot_radon_transform.html
+    theta = np.linspace(0., 180., min(image.shape), endpoint=False)
+    sinogram = radon(image, theta=theta)
+    return sinogram # просто получение синограммы с помощью библиотечной функции
 
 
 def discrete_radon_transform(image):
-    steps = len(sum(rotate(image, 0)))
-    R = np.zeros((len(image), steps), dtype='float64')
-    image = np.transpose(image)
+    steps = len(sum(rotate(image, 0))) # я не знаю что это и зачем
+    sinogram = np.zeros((len(image), steps), dtype='float64')
+    image = np.transpose(image)  # финт ушами чтобы все работало
+    # rotation = rotate(image, 0)
     for s in range(steps):
-        #rotation = rotate(image, -s*180/steps)
-        #rotation = rotate(image, 90)
-
-        rotation = rotate(image, 0)
-        R[:,s] = sum(rotation)
-    return R
+        rotation = rotate(image, s*180/steps + 90) # вращает изображение и возвращает что-то
+        sinogram[:,s] = sum(rotation) # я тоже не знаю что это...
+    return sinogram
 
 
 PATH = 'C:\\Users\\rybak\\Desktop\\Python programms\\MyProject\\mute_gray.png'
-# Read image as 64bit float gray scale
+
 image = cv2.imread(PATH, 0)
-radon = discrete_radon_transform(image)
+discrete_sinogram = discrete_radon_transform(image)
+sinogram = radon_transform(image)
 
 # Plot the original and the radon transformed image
-plt.subplot(1, 2, 1), plt.imshow(image, cmap='gray')
+plt.subplot(1, 3, 1), plt.imshow(image, cmap='gray')
 plt.xticks([]), plt.yticks([])
-plt.subplot(1, 2, 2), plt.imshow(radon, cmap='gray')
+plt.subplot(1, 3, 2), plt.imshow(discrete_sinogram, cmap='gray')
 plt.xticks([]), plt.yticks([])
+plt.subplot(1, 3, 3), plt.imshow(sinogram, cmap='gray')
+plt.xticks([]), plt.yticks([])
+plt.subplot(1, 3, 1), plt.title("Original picture")
+plt.subplot(1, 3, 2), plt.title("Discrete radon transformation")
+plt.subplot(1, 3, 3), plt.title("Radon transformation")
 plt.show()
